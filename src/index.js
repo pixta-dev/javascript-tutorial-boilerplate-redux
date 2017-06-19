403320c37193297fae067ffcbb9fc0697ba7d720
Yuji Suzuki <y0suzuki0123@gmail.com>
2017-06-19 16:28:23 +0900

    Return the state otherwise

--- src/index.js
+++ src/index.js
@@ -7,10 +7,13 @@ function counter(state, action) {
       return state + 1;
     case 'DECREMENT':
       return state - 1;
+    default:
+      return state;
   }
 }
 
 expect(counter(0, { type: 'INCREMENT' })).toEqual(1);
 expect(counter(1, { type: 'INCREMENT' })).toEqual(2);
 expect(counter(2, { type: 'DECREMENT' })).toEqual(1);
+expect(counter(1, { type: 'UNKNOWN' })).toEqual(1);
 console.log('all test passed!');
