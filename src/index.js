6ab242d0736155acd884f4bd285fe15e6a3da67b
Yuji Suzuki <y0suzuki0123@gmail.com>
2017-06-19 16:24:33 +0900

    Add an INCREMENT action

--- src/index.js
+++ src/index.js
@@ -2,7 +2,12 @@ import expect from 'expect';
 import './index.css';
 
 function counter(state, action) {
-  return state;
+  switch (action.type) {
+    case 'INCREMENT':
+      return state + 1;
+  }
 }
 
+expect(counter(0, { type: 'INCREMENT' })).toEqual(1);
+expect(counter(1, { type: 'INCREMENT' })).toEqual(2);
 console.log('all test passed!');
