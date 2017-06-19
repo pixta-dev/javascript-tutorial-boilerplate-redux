70bed755acfbcc720499f0a97745147141229ecf
Yuji Suzuki <y0suzuki0123@gmail.com>
2017-06-19 16:28:11 +0900

    Add a DECREMENT action

--- src/index.js
+++ src/index.js
@@ -5,9 +5,12 @@ function counter(state, action) {
   switch (action.type) {
     case 'INCREMENT':
       return state + 1;
+    case 'DECREMENT':
+      return state - 1;
   }
 }
 
 expect(counter(0, { type: 'INCREMENT' })).toEqual(1);
 expect(counter(1, { type: 'INCREMENT' })).toEqual(2);
+expect(counter(2, { type: 'DECREMENT' })).toEqual(1);
 console.log('all test passed!');
