838681b648a74a7edeffd81171d540a0e5668d74
Yuji Suzuki <y0suzuki0123@gmail.com>
2017-06-20 17:22:48 +0900

    17. React Todo List Example (Adding a Todo) (Write React-specific JS)

--- src/index.js
+++ src/index.js
@@ -1,12 +1,39 @@
 import { createStore } from 'redux';
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import todoApp from './reducers/todoApp';
 
 const store = createStore(todoApp);
 
+let nextTodoId = 0;
+export default class TodoApp extends Component {
+  render() {
+    return (
+      <div>
+        <button
+          onClick={() => {
+            store.dispatch({
+              type: 'ADD_TODO',
+              text: 'Test',
+              id: nextTodoId++,
+            });
+          }}
+        >
+          Add Todo
+        </button>
+        <ul>
+          {this.props.todos.map(todo => <li key={todo.id}>{todo.text}</li>)}
+        </ul>
+      </div>
+    );
+  }
+}
+
 function render() {
-  ReactDOM.render(<div />, document.getElementById('root'));
+  ReactDOM.render(
+    <TodoApp todos={store.getState().todos} />,
+    document.getElementById('root'),
+  );
 }
 
 store.subscribe(render);
