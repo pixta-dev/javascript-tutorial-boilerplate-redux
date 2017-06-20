a51a692ac22a68bda01662df7ba801e05556a260
Yuji Suzuki <y0suzuki0123@gmail.com>
2017-06-20 15:49:59 +0900

    09. Avoiding Array Mutations

--- src/avoiding-array-mutations.js
+++ src/avoiding-array-mutations.js
@@ -13,5 +13,31 @@ function testAddCounter() {
   expect(addCounter(listBefore)).toEqual(listAfter);
 }
 
+function removeCounter(list, index) {
+  return [...list.slice(0, index), ...list.slice(index + 1)];
+}
+function testRemoveCounter() {
+  const listBefore = [0, 10, 20];
+  const listAfter = [0, 20];
+
+  deepFreeze(listBefore);
+
+  expect(removeCounter(listBefore, 1)).toEqual(listAfter);
+}
+
+function incrementCounter(list, index) {
+  return [...list.slice(0, index), list[index] + 1, ...list.slice(index + 1)];
+}
+function testIncrementCounter() {
+  const listBefore = [0, 10, 20];
+  const listAfter = [0, 11, 20];
+
+  deepFreeze(listBefore);
+
+  expect(incrementCounter(listBefore, 1)).toEqual(listAfter);
+}
+
 testAddCounter();
+testRemoveCounter();
+testIncrementCounter();
 console.log('[avoiding-array-mutations] all tests passed.');
