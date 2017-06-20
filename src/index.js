016648ada32010e847a419aed1817ac335b68ee2
Yuji Suzuki <y0suzuki0123@gmail.com>
2017-06-20 16:56:34 +0900

    Prepare todo app

--- src/index.js
+++ src/index.js
@@ -1,51 +1,12 @@
-import expect from 'expect';
 import { createStore } from 'redux';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './avoiding-array-mutations';
-import './avoiding-object-mutations';
-import './writing-a-todo-list-reducer';
-import './index.css';
+import todoApp from './reducers/todoApp';
 
-function counter(state = 0, action) {
-  switch (action.type) {
-    case 'INCREMENT':
-      return state + 1;
-    case 'DECREMENT':
-      return state - 1;
-    default:
-      return state;
-  }
-}
-
-expect(counter(0, { type: 'INCREMENT' })).toEqual(1);
-expect(counter(1, { type: 'INCREMENT' })).toEqual(2);
-expect(counter(2, { type: 'DECREMENT' })).toEqual(1);
-expect(counter(1, { type: 'UNKNOWN' })).toEqual(1);
-expect(counter(undefined, {})).toEqual(0);
-console.log('[index.js] all test passed!');
-
-const store = createStore(counter);
-
-function Counter({ value, onIncrement, onDecrement }) {
-  return (
-    <div>
-      <h1>{value}</h1>
-      <button onClick={onIncrement}>+</button>
-      <button onClick={onDecrement}>-</button>
-    </div>
-  );
-}
+const store = createStore(todoApp);
 
 function render() {
-  ReactDOM.render(
-    <Counter
-      value={store.getState()}
-      onIncrement={() => store.dispatch({ type: 'INCREMENT' })}
-      onDecrement={() => store.dispatch({ type: 'DECREMENT' })}
-    />,
-    document.getElementById('root'),
-  );
+  ReactDOM.render(<div />, document.getElementById('root'));
 }
 
 store.subscribe(render);
---
+++ src/reducers/todoApp.js
@@ -0,0 +1,112 @@
+import expect from 'expect';
+import deepFreeze from 'deep-freeze';
+
+function visibilityFilter(state = 'SHOW_ALL', action) {
+  switch (action.type) {
+    case 'SET_VISIBILITY_FILTER':
+      return action.filter;
+    default:
+      return state;
+  }
+}
+
+function todo(state, action) {
+  switch (action.type) {
+    case 'ADD_TODO':
+      return {
+        id: action.id,
+        text: action.text,
+        completed: false,
+      };
+    case 'TOGGLE_TODO':
+      if (state.id !== action.id) {
+        return state;
+      }
+
+      return {
+        ...state,
+        completed: !state.completed,
+      };
+    default:
+      return state;
+  }
+}
+
+function todos(state = [], action) {
+  switch (action.type) {
+    case 'ADD_TODO':
+      return [...state, todo(undefined, action)];
+    case 'TOGGLE_TODO':
+      return state.map(t => todo(t, action));
+    default:
+      return state;
+  }
+}
+function testAddTodo() {
+  const stateBefore = [];
+  const action = {
+    type: 'ADD_TODO',
+    id: 0,
+    text: 'Learn Redux',
+  };
+  const stateAfter = [
+    {
+      id: 0,
+      text: 'Learn Redux',
+      completed: false,
+    },
+  ];
+
+  deepFreeze(stateBefore);
+  deepFreeze(action);
+
+  expect(todos(stateBefore, action)).toEqual(stateAfter);
+}
+function testToggleTodo() {
+  const stateBefore = [
+    {
+      id: 0,
+      text: 'Learn Redux',
+      completed: false,
+    },
+    {
+      id: 1,
+      text: 'Go Shopping',
+      completed: false,
+    },
+  ];
+  const action = {
+    type: 'TOGGLE_TODO',
+    id: 1,
+  };
+  const stateAfter = [
+    {
+      id: 0,
+      text: 'Learn Redux',
+      completed: false,
+    },
+    {
+      id: 1,
+      text: 'Go Shopping',
+      completed: true,
+    },
+  ];
+
+  deepFreeze(stateBefore);
+  deepFreeze(action);
+
+  expect(todos(stateBefore, action)).toEqual(stateAfter);
+}
+
+function todoApp(state = {}, action) {
+  return {
+    todos: todos(state.todos, action),
+    visibilityFilter: visibilityFilter(state.visibilityFilter, action),
+  };
+}
+
+testAddTodo();
+testToggleTodo();
+console.log('[todoApp] all tests passed.');
+
+export default todoApp;
