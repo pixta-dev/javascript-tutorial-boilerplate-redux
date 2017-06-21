dd1c8707a1c620efbb85d96b8abef9192d947647
Yuji Suzuki <y0suzuki0123@gmail.com>
2017-06-21 20:17:07 +0900

    23. Extracting Container Components (VisibleTodoList, AddTodo)
    - Refactor `TodoApp` component

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import AddTodo from './components/AddTodo';
 import Footer from './components/Footer';
 import VisibleTodoList from './components/VisibleTodoList';
-import store from './store';
 
 export default function TodoApp() {
   return (
@@ -15,13 +14,4 @@ export default function TodoApp() {
   );
 }
 
-function render() {
-  ReactDOM.render(
-    <TodoApp {...store.getState()} />,
-    document.getElementById('root'),
-  );
-}
-
-store.subscribe(render);
-
-render();
+ReactDOM.render(<TodoApp />, document.getElementById('root'));
